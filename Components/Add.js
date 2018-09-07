6dd924f7becea5aa84fd2bbecffb41fd6aeb3563
miguelhasbun <miguelneptaly@gmail.com>
2018-09-06 19:23:02 -0600

    Por fin inputdialog

--- Components/Add.js
+++ Components/Add.js
@@ -3,14 +3,13 @@ import {StyleSheet, Text, View } from 'react-native';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+
 export default class Add extends React.Component {
     render() {
       return (
-          <ActionButton buttonColor="rgba(231,76,60,1)">
-            <ActionButton.Item buttonColor='#9b59b6' title="New Task">
-            <Icon name="md-create" style={styles.actionButtonIcon} />
-            </ActionButton.Item>
-          </ActionButton>
+        
+        <ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => this.props.func()}/> 
+        
       );
     }
   }
--- Components/All.js
+++ Components/All.js
@@ -2,14 +2,22 @@ import React from 'react';
 import {Text, View } from 'react-native';
 import Add from './Add';
 import {Container, Button,Body,Title,Right,Header,Left} from 'native-base';
-
-
+import DialogInput from 'react-native-dialog-input';
 
 export default class All extends React.Component {
-   
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
+   constructor(props){
+       super(props);
+       this.state={
+           loading:true,
+           isDialogVisible: false
+       }
+       this.onPress=this.onPress.bind(this);
+       
+   }
+  onPress(){
+    this.setState({
+        isDialogVisible: true
+    })
   }
 
   async componentWillMount() {
@@ -34,8 +42,17 @@ export default class All extends React.Component {
       </Header>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>All!</Text>
-      <Add/>    
+      <Add func = {this.onPress}/>    
       </View> 
+      
+      <DialogInput isDialogVisible={this.state.isDialogVisible}
+            title={"Agregar nueva Tarea"}
+            hintInput ={"Ingrese nueva tarea"}
+            submitInput={(inputedText) => {this.handleChangedText(inputedText)}}
+            closeDialog={ () => {this.setState({isDialogVisible: false})}}>
+        </DialogInput>
+ 
+      
     </Container> 
     )
 }
